5e9d146c3131c8b054afb42a81164e953964a234
Jonnathan Leitao <leitaojonnathan@gmail.com>
2024-09-24 18:02:07 -0300

    vendo operadores logicos em python

---
+++ Part1/Conhecendo a linguagem/operadores.py
@@ -0,0 +1,8 @@
+print(5+8)
+
+print(5-8)
+
+print(5*8)
+
+print(5/8)
+print(5//8)
---
+++ Part1/Conhecendo a linguagem/operadores_atribuicao.py
@@ -0,0 +1,11 @@
+saldo = 500
+saldo -=200
+print(saldo)
+saldo += 200
+print(saldo)
+
+saldo /= 2
+print(saldo)
+
+saldo //= 2
+print(saldo)
